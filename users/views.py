2c4b0feed4c6bb051b128658223eb3daba0a620e
celeste-6 <sage.celeste6@gmail.com>
2022-04-29 21:04:10 +0900

    fix: edit order of import files

--- users/views.py
+++ users/views.py
@@ -1,11 +1,12 @@
 import bcrypt
 import jwt
 
-from datetime               import datetime, timedelta
 from django.http            import JsonResponse
 from django.views           import View
+
 from users.models           import User
 from my_settings            import ALGORITHM, SECRET_KEY
+from datetime               import datetime, timedelta
 
 class SignInView(View):
     def post(self, request):
@@ -14,20 +15,23 @@ class SignInView(View):
         try:
             email          = data['email']
             user           = User.objects.get(email=email)
-            user_saved_pwd = user.password
-            password       = data['password']
+            password = data['password']
 
-            if not bcrypt.checkpw(password.encode('utf-8'), user_saved_pwd.encode('utf-8')):
+            if not bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
 
                 current_time          = datetime.utcnow()
                 expiration_time       = timedelta(weeks=3)
                 token_expiration_time = current_time+expiration_time
-                jwt_access_token      = jwt.encode({'id':user.id, 'exp':token_expiration_time}, SECRET_KEY, ALGORITHM)
+                payload = {
+                    'id' : user.id,
+                    'exp' : datetime.utcnow() + timedelta(weeks=3)
+                }
+                jwt_access_token = jwt.encode(payload, SECRET_KEY, ALGORITHM)
 
                 return JsonResponse({'messasge':'SUCCESS','JWT_TOKEN':jwt_access_token}, status=200)
-            return JsonResponse({"message":"INCORRECT_PASSWORD"},status=401)
+            return JsonResponse({"message":"INVALID_USER"},status=401)
 
         except KeyError:
             return JsonResponse({"message":"KEY_ERROR"},status=400)
         except User.DoesNotExist:
-            return JsonResponse({"message":"NOT_REGISTERED_EMAIL"},status=401)
+            return JsonResponse({"message":"INVALID_USER"},status=401)
