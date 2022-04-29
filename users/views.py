e47a515654ce6c0615ae1d22ac38a019806a74cc
celeste-6 <sage.celeste6@gmail.com>
2022-04-29 13:15:51 +0900

    feat: Create SignUpView & url settings

--- users/views.py
+++ users/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+import bcrypt
+
+from django.core.exceptions import ValidationError
+from users.validation import validate_email, existing_email, validate_password
+
+from django.http      import HttpResponse
+from django.views     import View
+from users.models     import User
+
+class SignUpView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+
+        try:
+            email = data['email']
+            firstname = data['firstname']
+            lastname = data['lastname']
+            password = data['password']
+
+            validate_email(email)
+            existing_email(email)
+            validate_password(password)
+
+            hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+            decoded_hashed_password = hashed_password.decode('utf-8')
+
+            User.object.create(
+                email = email,
+                firstname = firstname,
+                lastname = lastname,
+                password = decoded_hashed_password,
+            )
+            return JsonResponse({"message": "SUCCESS"}, status=201)
+
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
+
+        except ValidationError as error:
+            return JsonResponse({'message' : error.message}, status=error.code)
