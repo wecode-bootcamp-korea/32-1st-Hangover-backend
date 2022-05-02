6feec8dabab07f15cb4212f901515d22c3a93325
celeste-6 <sage.celeste6@gmail.com>
2022-05-03 08:44:46 +0900

    Refactor: cleanup food_category and ave_rating after mentor review

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
-from django.views import View
-from django.http  import JsonResponse
+from django.views     import View
+from django.http      import JsonResponse
 from django.db.models import Avg
 
 from products.models import Product
