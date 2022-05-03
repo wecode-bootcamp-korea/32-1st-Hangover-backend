df89e7e8b87cca82161156b21f86dbced1270fb1
YoungbinKim <colock123@gmail.com>
2022-05-03 11:55:00 +0900

    main_merged

--- hangover/urls.py
+++ hangover/urls.py
@@ -1,11 +1,6 @@
 from django.urls    import path, include
-
 urlpatterns = [
-<<<<<<< HEAD
-    
-]
-=======
     # path('users', include('users.urls')),
     path('products', include('products.urls')),
     # path('reviews', include('reviews.urls')),
->>>>>>> 2c505a93012d8d6d897e52425b02eaa7bc7485af
+]
