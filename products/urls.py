1b67aa74d839f80f6f5d1028602b7b3911a567c8
YoungbinKim <colock123@gmail.com>
2022-05-03 17:16:46 +0900

    migration fixed

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,5 @@ from products.views import ProductDetailView,ProductSearchView
 
 urlpatterns = [
     path('/<int:product_id>', ProductDetailView.as_view()),
-    path('/search',views.ProductSearchView.as_view()),
+    path('/search',ProductSearchView.as_view()),
 ]
