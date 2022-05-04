d68c4ff53ab2506c0315b1a070b5b2c602c08042
celeste-6 <sage.celeste6@gmail.com>
2022-05-04 14:49:02 +0900

    Fix: receive product_id by body, not by query string

--- reviews/views.py
+++ reviews/views.py
@@ -4,23 +4,28 @@ from django.views    import View
 from django.http     import JsonResponse
 
 from core.utils     import login_decorator
-from reviews.models  import Review, ReviewLike
+from reviews.models import Review
 
 class ReviewView(View):
     def get(self, request):
-        data = json.loads(request.body)
+        try:
+            data = json.loads(request.body)
 
-        product_id = data.get('product_id')
-        reviews = Review.objects.filter(id=product_id).all()
+            product_id = data['product_id']
+            
+            reviews = Review.objects.filter(id=product_id).all()
 
-        review_list = [{
-            "firstname"  : review.user.firstname,
-            "lastname"   : review.user.lastname,
-            "user_id"    : review.user.id,
-            "content"    : review.content,
-            "rating"     : review.rating.score,
-            "created_at" : review.created_at,
-            "review_id"  : review.id
-        } for review in reviews]
+            review_list = [{
+                "firstname"  : review.user.firstname,
+                "lastname"   : review.user.lastname,
+                "user_id"    : review.user.id,
+                "content"    : review.content,
+                "rating"     : review.rating.score,
+                "created_at" : review.created_at,
+                "review_id"  : review.id
+            } for review in reviews]
 
-        return JsonResponse({'Reviews':review_list}, status=200)
+            return JsonResponse({'Reviews':review_list}, status=200)
+        
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
