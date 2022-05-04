45a4e22103731d005bde1d886c25a681c43dfd50
celeste-6 <sage.celeste6@gmail.com>
2022-05-04 16:19:50 +0900

    Feat: Created ReviewView DELETE - delete reviews

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views    import View
+from django.http     import JsonResponse
+
+from core.utils      import login_decorator
+from reviews.models  import Review
+
+class ReviewView(View):
+    @login_decorator
+    def delete(self, request):
+        try:
+            data = json.loads(request.body)
+            review_id = data['review_id']
+
+            review = Review.objects.get(
+                id = review_id
+            )
+            if request.user.id == review.user.id:
+                review.delete()
+
+            return JsonResponse({"message": "Review was deleted"}, status = 200)
+        
+        except Review.DoesNotExist:
+            return JsonResponse({"message" : "INVALID_REVIEW"}, status=401)
+
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
