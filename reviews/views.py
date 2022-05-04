62b6175a88cdac95ad8b9fc744c44b83e858ea94
celeste-6 <sage.celeste6@gmail.com>
2022-05-04 16:10:56 +0900

    Feat: Created ReviewView PATCH - update reviews

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views    import View
+from django.http     import JsonResponse
+
+from core.utils      import login_decorator
+from reviews.models  import Review
+
+class ReviewView(View):
+    @login_decorator
+    def patch(self, request): 
+        data = json.loads(request.body)
+        user = request.user
+
+        review_id = data['review_id']
+
+        review = Review.objects.get(
+            id = review_id
+            )
+
+        if review.user != request.user:
+            return JsonResponse({'message': 'INVALID_USER'}, status=401)
+
+        review.content = data.get('content', review.content)
+        review.save()
+
+        return JsonResponse({'message': 'SUCCESS'}, status=204)
