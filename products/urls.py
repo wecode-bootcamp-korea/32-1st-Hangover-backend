16e089d07eb35578c31a0bb4ca39b6814190204d
YoungbinKim <colock123@gmail.com>
2022-05-04 14:39:39 +0900

    Searchviewfixed

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
-
-from products.views import ProductDetailView
+from django.urls    import path,include
+from products.views import ProductDetailView,ProductSearchView
 
 urlpatterns = [
     path('/<int:product_id>', ProductDetailView.as_view()),
+    path('/search',ProductSearchView.as_view()),
 ]
--- products/views.py
+++ products/views.py
@@ -1,8 +1,57 @@
-from django.views     import View
-from django.http      import JsonResponse
-from django.db.models import Avg
+import random
 
-from products.models import Product
+from django.db.models import Avg
+from django.http      import JsonResponse
+from django.views     import View
+
+from products.models  import Product, Category, Country, FoodPairing
+
+class ProductSearchView(View):
+    def get(self,request):
+
+        search = request.GET.get('search')
+        limit  = int(request.GET.get('limit',10))
+
+        products_list = Product.objects.all().annotate(avg_rating= Avg('reviews__rating__score')).order_by('-avg_rating')
+
+        if search in Category.objects.all().values_list('name',flat = True):
+            products_list = products_list.filter(category_id__name=search)
+            filter = f'Category : {search}'
+
+        elif search in Country.objects.all().values_list('origin',flat = True):
+            products_list = products_list.filter(country_id__origin=search)
+            filter = f'Country : {search}'
+
+        elif search in FoodPairing.objects.all().values_list('food_category',flat = True):
+            products_list = products_list.filter(productfoodpairing__foodpairing__food_category=search)
+            filter = f'Foodpairing : {search}'
+        
+        else:
+            products_list = products_list.filter(name__icontains=search)
+            filter = None
+
+        if products_list:
+            result = [{
+                'id'           :product.id,
+                'price'        :product.price,
+                'name'         :product.name,
+                'country'      :product.country.origin,
+                'category'     :product.category.name,
+                'image_url'    :product.imageurl_set.first().image_url,
+                'created_at'   :product.created_at,
+                'rating'       :product.avg_rating, 
+            } for product in products_list][:limit]
+            return JsonResponse({"result":[result,filter]}, status=200)
+
+        else:
+            message = "no_searched_products"
+            recommended_words = {
+                "Category"    : random.choice(Category.objects.all().values_list('name', flat = True)),
+                "Country"     : random.choice(Country.objects.all().values_list('origin', flat = True)),
+                "Foodpairing" : random.choice(FoodPairing.objects.all().values_list('food_category', flat = True))
+                }
+            return JsonResponse({"result":[message,recommended_words]}, status=200)
+
 
 class ProductDetailView(View):
     def get(self, request, product_id):
@@ -18,7 +67,6 @@ class ProductDetailView(View):
                 'country'            : product.country.origin,
                 'alcohol_percentage' : product.alcohol_percentage,
                 'food_category'      : [food.food_category for food in product.food_category.all()],
-                'image_url'          : product.imageurl_set.first().image_url,
                 'property'           : product.property,
                 'reviews'            : product.reviews.all().count(),
                 'ave_rating'         : product.avg_rating
