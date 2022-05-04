02ec47c1b3050b0b93b746c9479f53cea53732c7
celeste-6 <sage.celeste6@gmail.com>
2022-05-04 14:08:12 +0900

    Feat: Created ReviewView POST - create reviews

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views    import View
+from django.http     import JsonResponse
+
+from core.utils     import login_decorator
+from reviews.models  import Review, ReviewLike
+
+class ReviewView(View):
+
+    @login_decorator
+    def post(self, request):
+        data       = json.loads(request.body)
+
+        user       = request.user
+        product_id = request.GET.get('product_id')
+        content    = data.get('content')
+        rating     = data.get('rating')
+
+        if not (rating or content):
+            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
+
+        review = Review.objects.create(
+            user       = user,
+            product_id = product_id,
+            rating_id  = rating,
+            content    = content
+        )
+
+        return JsonResponse({'message': 'SUCCESS'}, status=201)
