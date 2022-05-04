27cf664c734672ec23904ce2c6073982cfb01463
YoungbinKim <colock123@gmail.com>
2022-05-04 21:52:06 +0900

    fixed:filter

--- hangover/urls.py
+++ hangover/urls.py
@@ -4,3 +4,4 @@ urlpatterns = [
     # path('users', include('users.urls')),
     path('products', include('products.urls')),
     # path('reviews', include('reviews.urls')),
+]
--- products/views.py
+++ products/views.py
@@ -11,25 +11,30 @@ class ProductSearchView(View):
     2. 검색어가 상품명에 포함되는 상품 리스트를 반환 
     3. 일치되는 상품이 없을 경우, 추천검색어를 반환
     """
+
     def get(self,request):
 
         search = request.GET.get('search')
         limit  = int(request.GET.get('limit',10))
-
+        print(search)
+        print(type(search))
         keyword_list = {
             "Category"    : Category.objects.all().values_list('name',flat = True),
             "Country"     : Country.objects.all().values_list('origin',flat = True),
             "Foodpairing" : FoodPairing.objects.all().values_list('food_category',flat = True),
         }
-        
+
+        filter_dict = {}
+
         for key,values in keyword_list.items():
             for word in values:
                 if search in word:
-                    filter,matched_keyword = key,word
-
+                    filter_dict['filter'] = key
+                    matched_keyword = word
         products_list = Product.objects.all().annotate(avg_rating= Avg('reviews__rating__score')).order_by('-avg_rating')
-
-        if filter in globals():
+        filter = filter_dict.get('filter')
+        
+        if filter:
             Q_filter = {
                 'Category'        : Q(category_id__name=matched_keyword),
                 'Country'         : Q(country_id__origin=matched_keyword),
@@ -41,7 +46,7 @@ class ProductSearchView(View):
             products_list          = products_list.filter(name__icontains=search)
             filter,matched_keyword = None,None
 
-        if products_list:
+        if products_list.exists():
             result = [{
                 'id'           :product.id,
                 'price'        :product.price,
@@ -51,15 +56,16 @@ class ProductSearchView(View):
                 'image_url'    :product.imageurl_set.first().image_url,
                 'created_at'   :product.created_at,
                 'rating'       :product.avg_rating, 
-            } for product in products_list][:limit]
-            return JsonResponse({"response":[{"filter":filter,"matched_keyword":matched_keyword,"result":result}]}, status=200)
+            } for product in products_list.order_by("?")[:limit]]
+
+            return JsonResponse({"filter":filter,"matched_keyword":matched_keyword,"result":result}, status=200)
 
         else:
             message = "no_searched_products"
             recommended_words = {
                 "Category"    : Category.objects.all().values_list('name', flat = True).order_by('?').first(),
                 "Country"     : Country.objects.all().values_list('origin', flat = True).order_by('?').first(),
-                "Foodpairing" : FoodPairing.objects.all().values_list('food_category', flat = True).order_by('?').first()
+                "Foodpairing" : FoodPairing.objects.all().values_list('food_category', flat = True).first()
                 }
             return JsonResponse({"response":[message,recommended_words]}, status=200)
 
