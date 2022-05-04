3a35d1993f15ebf62789e6ae11cdcf9c15215914
celeste-6 <sage.celeste6@gmail.com>
2022-05-04 14:30:40 +0900

    Feat: Created ReviewView GET - view reviews

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+    def get(self, request):
+        data = json.loads(request.body)
+        
+        product_id = data.get('product_id')
+        reviews = Review.objects.filter(id=product_id).all()
 
-# Create your views here.
+        review_list = [{
+            "firstname"  : review.user.firstname,
+            "lastname"   : review.user.lastname,
+            "user_id"    : review.user.id,
+            "content"    : review.content,
+            "rating"     : review.rating.score,
+            "created_at" : review.created_at,
+            "review_id"  : review.id
+        } for review in reviews]
+
+        return JsonResponse({'Reviews':review_list}, status=200)
