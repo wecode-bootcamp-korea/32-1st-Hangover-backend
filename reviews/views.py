b554156dd06ab815093c90391cc4f8b4dc6a1a5f
celeste-6 <sage.celeste6@gmail.com>
2022-05-04 16:24:28 +0900

    Feat: invalid review & key error handling

--- reviews/views.py
+++ reviews/views.py
@@ -9,19 +9,26 @@ from reviews.models  import Review
 class ReviewView(View):
     @login_decorator
     def patch(self, request): 
-        data = json.loads(request.body)
-        user = request.user
+        try:
+            data = json.loads(request.body)
+            user = request.user
 
-        review_id = data['review_id']
+            review_id = data['review_id']
 
-        review = Review.objects.get(
-            id = review_id
-            )
+            review = Review.objects.get(
+                id = review_id
+                )
 
-        if review.user != request.user:
-            return JsonResponse({'message': 'INVALID_USER'}, status=401)
+            if review.user != request.user:
+                return JsonResponse({'message': 'INVALID_USER'}, status=401)
 
-        review.content = data.get('content', review.content)
-        review.save()
+            review.content = data.get('content', review.content)
+            review.save()
 
-        return JsonResponse({'message': 'SUCCESS'}, status=204)
+            return JsonResponse({'message': 'SUCCESS'}, status=204)
+
+        except Review.DoesNotExist:
+            return JsonResponse({"message" : "INVALID_REVIEW"}, status=401)
+
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
