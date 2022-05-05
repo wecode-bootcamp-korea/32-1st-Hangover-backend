09e896367c3e9a306fcee96e47ff6b21cc080834
celeste-6 <sage.celeste6@gmail.com>
2022-05-05 16:34:51 +0900

    Fix: change password validation

--- users/validation.py
+++ users/validation.py
@@ -5,7 +5,7 @@ from django.core.exceptions import ValidationError
 from users.models           import User
 
 REGEX_EMAIL    = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
-REGEX_PASSWORD = '^(?=.*[a-zA-Z])((?=.*\d)(?=.*\W)).{10,}$'
+REGEX_PASSWORD = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{10,}$'
 
 def validate_email(email):
     if not re.match(REGEX_EMAIL, email):
